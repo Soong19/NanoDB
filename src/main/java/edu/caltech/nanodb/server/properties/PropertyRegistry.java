b15ec1f7e034a551aa21b0e3de3fb3571c2c5052
Ludwig Huang <0711feiyu@gmail.com>
2022-12-05 12:31:57 +0800

    Enable Indexes

--- src/main/java/edu/caltech/nanodb/server/properties/PropertyRegistry.java
+++ src/main/java/edu/caltech/nanodb/server/properties/PropertyRegistry.java
@@ -133,10 +133,10 @@ public class PropertyRegistry implements ServerProperties {
             new BooleanFlagValidator(), true);
 
         addProperty(PROP_ENABLE_INDEXES,
-            new BooleanFlagValidator(), false, /* reaadonly */ true);
+            new BooleanFlagValidator(), true, /* reaadonly */ true);
 
         addProperty(PROP_CREATE_INDEXES_ON_KEYS,
-            new BooleanFlagValidator(), false);
+            new BooleanFlagValidator(), true);
 
         addProperty(PROP_PLANNER_CLASS,
             new PlannerClassValidator(), DEFAULT_PLANNER_CLASS);
